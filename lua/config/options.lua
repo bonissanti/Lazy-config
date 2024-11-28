b0a579633c88179e7be52f07413445a0735a4422
Bruno Onissanti <brunoonissanti@gmail.com>
2024-11-28 19:48:14 -0300

    bruh
    Add LazyVim Configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use actual tab characters instead of spaces
+vim.opt.expandtab = false
+
+-- Set the number of spaces for each tab
+vim.opt.tabstop = 4
+
+-- Set the number of spaces for automatic indentation
+vim.opt.shiftwidth = 4
+
+-- Enable automatic indentation
+vim.opt.autoindent = true
+
+vim.g.autoformat = false
